9d91a718c97f1f9c59aa055961b08bf02c247f2a
Sukum Duangpattra <tonypengfusama@gmail.com>
2020-11-20 16:50:57 +0700

    RefreshManager n BonusPanel Added

---
+++ MonsterPet/MainScene/BonusPanel.swift
@@ -0,0 +1,9 @@
+//
+//  BonusPanel.swift
+//  MonsterPet
+//
+//  Created by Sukum Duangpattra on 20/11/2563 BE.
+//  Copyright © 2563 BE Sukum Duangpattra. All rights reserved.
+//
+
+import Foundation
---
+++ MonsterPet/Utilities/RefreshManager.swift
@@ -0,0 +1,9 @@
+//
+//  RefreshManager.swift
+//  MonsterPet
+//
+//  Created by Sukum Duangpattra on 20/11/2563 BE.
+//  Copyright © 2563 BE Sukum Duangpattra. All rights reserved.
+//
+
+import Foundation
